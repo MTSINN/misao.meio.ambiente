cb8555ed87dcf9a25d5d11392583f6ca6e9b59b5
MTSINN <dalcol.lara@escola.pr.gov.br>
2026-08-19 12:08:37 -0300

    i

--- script.js
+++ script.js
@@ -18,5 +18,26 @@ const perguntas = [
             "Alternativa 1",
             "Alternativa 2"
         ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
     }
 ];
